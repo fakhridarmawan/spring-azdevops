84c75242d7da1ec456a2ecd79d4880dc599c3910
fakhri <fakhridarmawan@gmail.com>
2021-03-02 15:46:19 +0700

    init

---
+++ src/main/java/com/springframework/boot/springstarter/BlogController.java
@@ -0,0 +1,13 @@
+package com.springframework.boot.springstarter;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BlogController {
+  
+  @RequestMapping("/")
+  public String index() {
+    return "Congratulations";
+  }
+}
